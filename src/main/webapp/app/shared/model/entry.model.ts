ed3204a9528ca1a6227c02bd9dd2e42693d432e4
badriah8902 <badriahazmi@gmail.com>
2021-08-20 09:41:39 +0800

    Upload all files to github
    First commit to upload all file to github.

---
+++ src/main/webapp/app/shared/model/entry.model.ts
@@ -0,0 +1,31 @@
+import { Emoji } from 'app/shared/model/enumerations/emoji.model';
+
+export interface IEntry {
+  id?: number;
+  title?: string;
+  emoji?: Emoji;
+  content?: any;
+  blogName?: string;
+  blogId?: number;
+  blogPositive?: boolean;
+  createdBy?: string;
+  createdDate?: Date;
+  lastModifiedBy?: string;
+  lastModifiedDate?: Date;
+}
+
+export class Entry implements IEntry {
+  constructor(
+    public id?: number,
+    public title?: string,
+    public emoji?: Emoji,
+    public content?: any,
+    public blogName?: string,
+    public blogId?: number,
+    public blogPositive?: boolean,
+    public createdBy?: string,
+    public createdDate?: Date,
+    public lastModifiedBy?: string,
+    public lastModifiedDate?: Date
+  ) {}
+}
